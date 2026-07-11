305045fea676f3405a7c34ff7464df83ca0759ea
Favour Michael <michaelfav28@gmail.com>
2026-07-11 17:35:14 +0300

    Add Firebase initialization to style.css
    Initialized Firebase with configuration details.

---
+++ css/style.css
@@ -0,0 +1,11 @@
+// Firebase initialization
+firebase.initializeApp({
+  apiKey:"AIzaSyD4F8mPa3fOSqD_PNJz3MDoZcu32NO8ZxA",
+  authDomain:"duka-pro-2da7a.firebaseapp.com",
+  projectId:"duka-pro-2da7a",
+  storageBucket:"duka-pro-2da7a.firebasestorage.app",
+  messagingSenderId:"420819118777",
+  appId:"1:420819118777:web:c21b2ebe4a5e53c5648968"
+});
+const auth=firebase.auth();
+const db=firebase.firestore();
